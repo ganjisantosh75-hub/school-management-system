8ce0f94d0c625fdefceb34d9bec2dc8e6905aa5b
unknown <ganjisantosh75@gmail.com>
2026-07-07 13:52:21 +0530

    Debug API URL

--- src/config.js
+++ src/config.js
@@ -2,6 +2,12 @@
 
 // export default API_URL;
 
+// const API_URL = import.meta.env.VITE_API_URL;
+
+// export default API_URL;
+
 const API_URL = import.meta.env.VITE_API_URL;
+
+console.log("API_URL =", API_URL);
 
 export default API_URL;
