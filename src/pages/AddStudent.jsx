a905929f38c2e5732cb6cc7a1d7a32dd1bed6128
unknown <ganjisantosh75@gmail.com>
2026-09-11 15:36:31 +0530

    AddStudent,jsx

--- src/pages/AddStudent.jsx
+++ src/pages/AddStudent.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import AdminLayout from "../component/admin/AdminLayout";
 import API_URL from "../config";
-import { parseSync } from "vite";
+
 
 function AddStudent() {
   const navigate = useNavigate();
